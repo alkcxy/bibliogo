9b6bc63855a2f46c527bb61f9bc670e368ef432b
Alessio Caradossi <acaradossi@hotels.com>
2021-05-12 22:27:45 +0200

    Improved paginator, added book title on loan page, links between book and its loans

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -12,8 +12,8 @@ class Loan
   belongs_to :book
 
   def self.unloanable(loan_date, quarantine_date)
-    expected = where({ date: { '$lte' => loan_date }, expected_return: { '$gte' => quarantine_date }}).order_by(expected_return: :asc).first
-    actual = where({ date: { '$lte' => loan_date }, actual_return: { '$gte' => quarantine_date }}).order_by(actual_return: :asc).first
+    expected = self.expected(loan_date, quarantine_date).first
+    actual = self.actual(loan_date, quarantine_date).first
 
     if expected && (actual && expected.expected_return < actual.actual_return || !actual) && expected != actual
       expected.expected_return
@@ -24,4 +24,22 @@ class Loan
     end
 
   end
+
+  def self.lent(loan_date, quarantine_date)
+    expected = self.expected(loan_date, quarantine_date).first
+    actual = self.actual(loan_date, quarantine_date).first
+
+    if expected && (actual && expected.expected_return < actual.actual_return || !actual) && expected != actual
+      expected
+    elsif actual
+      actual
+    else
+      nil
+    end
+  end
+
+  scope :expected, -> (loan_date, quarantine_date) { where({ date: { '$lte' => loan_date }, expected_return: { '$gte' => quarantine_date }}).order_by(expected_return: :asc) }
+  scope :actual, -> (loan_date, quarantine_date) { where({ date: { '$lte' => loan_date }, actual_return: { '$gte' => quarantine_date }}).order_by(actual_return: :asc) }
+
+
 end
