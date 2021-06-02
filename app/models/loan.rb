0fd2c9d5ddb4f0d492d10ddfbf811b1f5384d75a
Alessio Caradossi <acaradossi@hotels.com>
2021-06-02 14:29:13 +0200

    fix end of loan not correctly handled

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -22,10 +22,10 @@ class Loan
     expected = self.expected(loan_date, quarantine_date).first
     actual = self.actual(loan_date, quarantine_date).first
 
-    if expected && (actual && expected.expected_return < actual.actual_return || !actual) && expected != actual
-      expected.expected_return
-    elsif actual
+    if actual
       actual.actual_return
+    elsif expected && !expected.actual_return
+      expected.expected_return
     else
       nil
     end
@@ -36,17 +36,17 @@ class Loan
     expected = self.expected(loan_date, quarantine_date).first
     actual = self.actual(loan_date, quarantine_date).first
 
-    if expected && (actual && expected.expected_return < actual.actual_return || !actual) && expected != actual
-      expected
-    elsif actual
+    if actual
       actual
+    elsif expected
+      expected
     else
       nil
     end
   end
 
-  scope :expected, -> (loan_date, quarantine_date) { where({ date: { '$lte' => loan_date }, expected_return: { '$gte' => quarantine_date }}).order_by(expected_return: :asc) }
-  scope :actual, -> (loan_date, quarantine_date) { where({ date: { '$lte' => loan_date }, actual_return: { '$gte' => quarantine_date }}).order_by(actual_return: :asc) }
+  scope :expected, -> (loan_date, quarantine_date) { where({ date: { '$lte' => loan_date }, expected_return: { '$gt' => quarantine_date }}).order_by(expected_return: :asc) }
+  scope :actual, -> (loan_date, quarantine_date) { where({ date: { '$lte' => loan_date }, actual_return: { '$gt' => quarantine_date }}).order_by(actual_return: :asc) }
 
 
 end
