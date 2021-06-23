21729d2b622b4184a7a21a26f74c36ca93955b35
Alessio Caradossi <acaradossi@hotels.com>
2021-06-23 19:51:08 +0200

    #8 added test for Loan.late scope

--- test/models/loan_test.rb
+++ test/models/loan_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class LoanTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
   setup do
     @book = create(:book_with_loans)
     @loan = @book.loans.first
@@ -22,20 +18,23 @@ class LoanTest < ActiveSupport::TestCase
     Setting.all.each do |setting|
       setting.destroy
     end
-
   end
 
   test "is not loanable" do
-    
     book = create(:book_with_loans)
     status = book.loans.status(Date.today, 3.days).first
     assert status.nil?
-
   end
 
   test "there isn't any active loan today" do
     book = create(:book_with_loans)
     loan = Loan.active(Date.today, 1.month.since(Date.today)).where(book_id: book.id).first
     assert loan.nil?
+  end
+
+  test "there is a late loan" do
+    @loan = create(:not_returned, book: create(:bambina))
+    loan = Loan.late(Date.today).first
+    assert loan == @loan
   end
 end
