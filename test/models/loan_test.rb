30970e5b9037197d8caeab4bcd786179c3a783a5
Alessio Caradossi <acaradossi@hotels.com>
2021-06-23 20:03:00 +0200

    #8 completing unit tests for checking status of a book/loan

--- test/models/loan_test.rb
+++ test/models/loan_test.rb
@@ -20,16 +20,36 @@ class LoanTest < ActiveSupport::TestCase
     end
   end
 
-  test "is not loanable" do
+  test "is loanable with old loans" do
     book = create(:book_with_loans)
     status = book.loans.status(Date.today, 3.days).first
     assert status.nil?
   end
 
+  test "is loanable without any loans yet" do
+    book = create(:bambina)
+    status = book.loans.status(Date.today, 3.days).first
+    assert status.nil?
+  end
+
+  test "is not loanable" do
+    @book = create(:bambina)
+    create(:in_loan, book: @book)
+    status = @book.loans.status(Date.today, 3.days).first
+    assert !status.nil?
+  end
+
   test "there isn't any active loan today" do
-    book = create(:book_with_loans)
+    book = create(:bambina)
     loan = Loan.active(Date.today, 1.month.since(Date.today)).where(book_id: book.id).first
     assert loan.nil?
+  end
+
+  test "there is an active loan today" do
+    @book = create(:bambina)
+    create(:in_loan, book: @book)
+    loan = Loan.active(Date.today, 1.month.since(Date.today)).where(book_id: @book.id).first
+    assert !loan.nil?
   end
 
   test "there is a late loan" do
@@ -40,5 +60,15 @@ class LoanTest < ActiveSupport::TestCase
 
     book = Book.late(Date.today).first
     assert book == @book
+  end
+
+  test "there isn't any late loans" do
+    @book = create(:bambina)
+    create(:lendable, book: @book)
+    loan = Loan.late(Date.today).first
+    assert loan.nil?
+
+    book = Book.late(Date.today).first
+    assert book.nil?
   end
 end
